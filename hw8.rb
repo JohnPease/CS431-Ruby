1d8953966e79522213c50a213cb093a4eabfb136
JohnPease <john.j.pease@gmail.com>
2014-03-23 14:18:07 -0500

    done with concatAll

--- hw8.rb
+++ hw8.rb
@@ -32,17 +32,42 @@ class BinaryNode
 
   # returns a single string that is all of a tree's strings concatenated left to right
   def concatAll
-
+    @left.concatAll.concat(@right.concatAll)
   end
 
   # returns the string in the whole tree that comes first alphabetically
   def firstAlphabetical
-
+    if (@left.firstAlphabetical.casecmp(@right.firstAlphabetical) == -1)
+      @left
+    else
+      @right
+    end
   end
 
   # takes a closure parameter and calls the closure with each string in the tree as an argument
-  def iterate
+  def iterate(cl)
 
   end
 
 end
+
+def test_tree
+  l0 = Leaf.new "What "
+  l1 = Leaf.new "a "
+  l2 = Leaf.new "great "
+  l3 = Leaf.new "day"
+  t0 = BinaryNode.new(l0,l1)
+  t1 = BinaryNode.new(t0,l2)
+  t2 = BinaryNode.new(t1,l3)
+
+  puts "tree concatenated:\t" + t2.concatAll
+  puts
+  puts "tree first alphabetical:\t" + t2.firstAlphabetical
+  puts
+  puts "tree iterated:"
+  puts
+  i = 0
+  t2.iterate(lambda { |s| i=i+1; puts i.to_s + ": " + s })
+end
+
+test_tree
